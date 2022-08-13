acb9d97174b4d2234989a74406bd448090f53442
youjheng <chagam0507@gmail.com>
2022-08-13 15:10:35 +0800

    add login feature via passport

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
+const usePassport = require('./config/passport')
+const passport = require('passport')
+
 const app = express()
 const PORT = 3000
 
@@ -20,6 +23,8 @@ app.use(session({
 }))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -41,9 +46,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
